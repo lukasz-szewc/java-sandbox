26b6d6b95c0d2cf729eec42d8c7eda5064942389
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-06-25 22:02:25 +0200

    added junit as dependency

---
+++ src/test/java/org/luksze/NewSyntaxTest.java
@@ -0,0 +1,12 @@
+package org.luksze;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NewSyntaxTest {
+    @Test
+    public void testName() throws Exception {
+        Assert.assertTrue(true);
+
+    }
+}
