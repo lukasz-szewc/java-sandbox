4c46bbe2e05b44e6a56cf3611f34a2ac6b2512aa
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-02-08 21:12:18 +0100

    another test method + refactor

--- src/main/java/org/luksze/DefaultMethodImplementation.java
+++ src/main/java/org/luksze/DefaultMethodImplementation.java
@@ -1,5 +1,15 @@
 package org.luksze;
 
-public class DefaultMethodImplementation implements DefaultMethodInterface {
+class DefaultMethodImplementation implements DefaultMethodInterface {
 
+    static class OverriddenDefaultMethodImplementation extends DefaultMethodImplementation {
+
+        public static final String OTHER_VALUE = "OtherValue";
+
+        @Override
+        public String defaultValue() {
+            return OTHER_VALUE;
+        }
+    }
 }
+
--- src/test/java/org/luksze/InterfaceDefaultMethodTest.java
+++ src/test/java/org/luksze/InterfaceDefaultMethodTest.java
@@ -1,17 +1,24 @@
 package org.luksze;
 
 import org.junit.Test;
+import org.luksze.DefaultMethodImplementation.OverriddenDefaultMethodImplementation;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.luksze.DefaultMethodImplementation.OverriddenDefaultMethodImplementation.OTHER_VALUE;
 import static org.luksze.DefaultMethodInterface.DEFAULT_VALUE;
-import static org.luksze.OverriddenDefaultMethodImplementation.OTHER_VALUE;
 
 public class InterfaceDefaultMethodTest {
 
     @Test
     public void canInheritDefaultMethodFromInterface() throws Exception {
         DefaultMethodInterface implementation = new DefaultMethodImplementation();
+        assertThat(implementation.defaultValue(), equalTo(DEFAULT_VALUE));
+    }
+
+    @Test
+    public void canCreateAnonymousImplementaterWithDefaultMethod() throws Exception {
+        DefaultMethodInterface implementation = new DefaultMethodInterface() {};
         assertThat(implementation.defaultValue(), equalTo(DEFAULT_VALUE));
     }
 
