e6af3300ec848d114330f61ec5f5fa55c1fbbe3f
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-02-10 18:56:55 +0100

    for each test in case of maps

--- src/test/java/org/luksze/ForEachTest.java
+++ src/test/java/org/luksze/ForEachTest.java
@@ -1,25 +1,53 @@
 package org.luksze;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ForEachTest {
     @Test
     public void simpleForEachOverListTest() throws Exception {
         //given
-        List<Integer> sourceList = Arrays.asList(1, 2, 3);
+        List<Integer> sourceList = asList(1, 2, 3);
         List<Integer> destinationList = new ArrayList<>();
 
         //when
-        sourceList.forEach(integer -> destinationList.add(integer));
+        sourceList.forEach(integer -> destinationList.add(integer * 2));
 
         //then
-        Assert.assertTrue(sourceList.containsAll(destinationList));
-        Assert.assertTrue(destinationList.containsAll(sourceList));
+        assertTrue(destinationList.contains(2));
+        assertTrue(destinationList.contains(4));
+        assertTrue(destinationList.contains(6));
 
+    }
+
+    @Test
+    public void simpleForEachOverMapTest() throws Exception {
+        //given
+        Map<String, Integer> map = mapWithThreeKeys();
+        AtomicInteger amount = new AtomicInteger();
+
+        //when
+        map.forEach((s, integer) -> amount.addAndGet(integer));
+
+        //then
+        assertEquals(6, amount.get());
+
+    }
+
+    private Map<String, Integer> mapWithThreeKeys() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("first", 1);
+        map.put("second", 2);
+        map.put("third", 3);
+        return map;
     }
 }
