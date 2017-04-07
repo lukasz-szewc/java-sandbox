c1e1ce9085e76a106646f6ae955fddbc1631b16a
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-08 00:25:37 +0200

    coverage fun again

--- src/test/java/org/luksze/SomeNotFullyCoveredClassTest.java
+++ src/test/java/org/luksze/SomeNotFullyCoveredClassTest.java
@@ -2,40 +2,9 @@ package org.luksze;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SomeNotFullyCoveredClassTest {
-
-    @Test
-    public void shouldHitFirstCondition() throws Exception {
-        SomeNotFullyCoveredClass sut = new SomeNotFullyCoveredClass();
-
-        boolean string = sut.complexCondition("abc");
-
-        assertFalse(string);
-
-    }
-
-    @Test
-    public void shouldHitSecondCondition() throws Exception {
-        SomeNotFullyCoveredClass sut = new SomeNotFullyCoveredClass();
-
-        boolean string = sut.complexCondition("abcdef");
-
-        assertFalse(string);
-
-    }
-
-    @Test
-    public void shouldHitThirdCondition() throws Exception {
-        SomeNotFullyCoveredClass sut = new SomeNotFullyCoveredClass();
-
-        boolean string = sut.complexCondition("boston");
-
-        assertTrue(string);
-
-    }
 
     @Test
     public void shouldHitThirdAndFourtCondition() throws Exception {
