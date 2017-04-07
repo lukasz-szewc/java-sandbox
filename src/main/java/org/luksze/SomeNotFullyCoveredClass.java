601e003a96fc419327e4b101efd1205ea09b4867
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-04-08 00:13:34 +0200

    coverage after change of condition

--- src/main/java/org/luksze/SomeNotFullyCoveredClass.java
+++ src/main/java/org/luksze/SomeNotFullyCoveredClass.java
@@ -2,6 +2,6 @@ package org.luksze;
 
 public class SomeNotFullyCoveredClass {
     public boolean complexCondition(String someText) {
-        return someText != null && someText.length() > 5 && (someText.contains("o") || someText.contains("s"));
+        return someText != null && someText.length() > 5 && (someText.contains("o") | someText.contains("s"));
     }
 }
