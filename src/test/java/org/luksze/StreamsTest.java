edcec1fff7c019f7c387bfe4d1e3eeecdaae82d1
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-05-08 21:48:17 +0200

    add another stream example

--- src/test/java/org/luksze/StreamsTest.java
+++ src/test/java/org/luksze/StreamsTest.java
@@ -2,9 +2,14 @@ package org.luksze;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import static java.lang.Character.valueOf;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StreamsTest {
 
@@ -21,5 +26,51 @@ public class StreamsTest {
 
         //then
         assertArrayEquals(sortedCharsLessThenG, new Character[]{'a', 'c', 'e'});
+    }
+
+    @Test
+    public void assertHorizontalOrderOfInvocationInsideStream() {
+        //given
+        LinkedList<String> invocationList = new LinkedList<>();
+
+        //when
+        Stream.of('a', 'b').map(character -> {
+            String string = character.toString();
+            invocationList.addLast(string);
+            return string;
+        }).filter(string -> {
+            invocationList.addLast(string);
+            return true;
+        }).toArray(String[]::new);
+
+        //then
+        assertEquals(invocationList.size(), 4);
+        assertEquals(invocationList.pollFirst(), "a");
+        assertEquals(invocationList.pollFirst(), "a");
+        assertEquals(invocationList.pollFirst(), "b");
+        assertEquals(invocationList.pollFirst(), "b");
+        assertTrue(invocationList.isEmpty());
+    }
+
+    @Test
+    public void orderOfInvocationsMatter() throws Exception {
+        //given
+        LinkedList<Character> invocationList = new LinkedList<>();
+
+        //when
+        Stream.of('a', 'b').filter(character -> {
+            invocationList.addLast(character);
+            return character != 'a';
+        }).map(character -> {
+            invocationList.addLast(character);
+            return character.toString();
+        }).toArray(String[]::new);
+
+        //then
+        assertEquals(invocationList.size(), 3);
+        assertEquals(invocationList.pollFirst(), valueOf('a'));
+        assertEquals(invocationList.pollFirst(), valueOf('b'));
+        assertEquals(invocationList.pollFirst(), valueOf('b'));
+        assertTrue(invocationList.isEmpty());
     }
 }
