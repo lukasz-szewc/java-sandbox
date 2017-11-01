3e23c7cc5ca06fe1b048fcb801fb05899b90d2f0
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2017-11-01 18:29:21 +0100

    added missing hashcode to year of life value object

--- src/main/java/org/luksze/YearOfLife.java
+++ src/main/java/org/luksze/YearOfLife.java
@@ -1,5 +1,7 @@
 package org.luksze;
 
+import java.util.Objects;
+
 class YearOfLife {
     private int age;
 
@@ -21,6 +23,11 @@ class YearOfLife {
         }
         YearOfLife that = (YearOfLife) o;
         return age == that.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
     }
 
     boolean isOlder(YearOfLife yearOfLife) {
