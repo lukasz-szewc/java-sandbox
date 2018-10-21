3b4daa3ca0ff4d9259ffa26d8086b20953c12483
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2018-10-21 20:39:46 +0200

    inlined constraint into method

--- src/main/java/org/luksze/DefaultMethodInterface.java
+++ src/main/java/org/luksze/DefaultMethodInterface.java
@@ -2,9 +2,7 @@ package org.luksze;
 
 public interface DefaultMethodInterface {
 
-    String DEFAULT_VALUE = "defaultValue";
-
     default String defaultValue() {
-        return DEFAULT_VALUE;
+        return "defaultValue";
     }
 }
--- src/test/java/org/luksze/InterfaceDefaultMethodTest.java
+++ src/test/java/org/luksze/InterfaceDefaultMethodTest.java
@@ -5,24 +5,25 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.luksze.OverriddenDefaultMethodImplementation.OTHER_VALUE;
-import static org.luksze.DefaultMethodInterface.DEFAULT_VALUE;
 
 public class InterfaceDefaultMethodTest {
 
+    private static final String DEFAULT_VALUE = "defaultValue";
+
     @Test
-    public void canInheritDefaultMethodFromInterface() throws Exception {
+    public void canInheritDefaultMethodFromInterface() {
         DefaultMethodInterface implementation = new DefaultMethodImplementation();
         assertThat(implementation.defaultValue(), equalTo(DEFAULT_VALUE));
     }
 
     @Test
-    public void canCreateAnonymousImplementationWithDefaultMethod() throws Exception {
+    public void canCreateAnonymousImplementationWithDefaultMethod() {
         DefaultMethodInterface implementation = new DefaultMethodInterface() {};
         assertThat(implementation.defaultValue(), equalTo(DEFAULT_VALUE));
     }
 
     @Test
-    public void canOverrideDefaultMethodFromInterface() throws Exception {
+    public void canOverrideDefaultMethodFromInterface() {
         DefaultMethodInterface implementation = new OverriddenDefaultMethodImplementation();
         assertThat(implementation.defaultValue(), equalTo(OTHER_VALUE));
     }
